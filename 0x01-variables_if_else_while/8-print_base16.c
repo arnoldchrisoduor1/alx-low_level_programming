862d836d8751e14721e7a8203008114a7523c4be
arnoldchrisoduor1 <109024629+arnoldchrisoduor1@users.noreply.github.com>
2022-09-09 21:53:42 +0300

    prints all the numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 /**
- * main - Prints reversed alphabet.
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
-for (i = 16; i > 0; i--)
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (i = 97; i < 103; i++)
 {
 putchar(i);
 }
