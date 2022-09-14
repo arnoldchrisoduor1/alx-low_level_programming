f55f7414f376b74f6dd5e0f583215557bb8463eb
arnoldchrisoduor1 <109024629+arnoldchrisoduor1@users.noreply.github.com>
2022-09-14 20:35:54 +0300

    prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,22 +5,21 @@
  * Description: print +, 0, or - depending on number, along with return
  * Return: 1 if +, 0 if 0, and -1 if -
  */
-
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
-	{
-		_putchar('0');
-		return (0);
-	}
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
 }
